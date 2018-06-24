ebf92faa05ac592432e2e574493b8092b238078a
lmaocow <lolcow@protonmail.com>
2018-06-24 20:29:50 +0000

    zzz

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ def main():
     f = Figlet(font='slant')
     print(f.renderText('kracca'))
     print("by six & lolcow")
+    if len(sys.argv) == 0:
+        print("must provide mode:\n--enterprise:    includes address and differing name permutations in results\n--personal:    includes DOB and family permutations")
+    else: 
+        if sys.argv[1] == "--enterprise": 
+            print("====ENTERPRISE MODE====")
+        elif sys.argv[1] == "--personal": 
+            print("====PERSONAL MODE====")
+        else: 
+            print("choose a mode, dummy.\n")
+        result  = sys.argv[2] 
+        keywords = sys.argv[3] 
+        keynums = sys.argv[4] 
     if sys.argv[1] == "--enterprise": 
-        print("====ENTERPRISE MODE====")
-    elif sys.argv[1] == "--personal": 
-        print("====PERSONAL MODE====")
-    keynums = input("keynum file: ")
-    keywords = input("keyword file: ")
-    results = input("result file: ") 
-    i = Info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), dob=input("dob: "), email=input("email: "), family=input("family: "), mode=sys.argv[1])
+        i = Info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise")
+    elif sys.argv[1] == "--personal":
+        i = Info(name=input("name: "), address=input("address: "), phone=input("phone: "), email=input("email: "), mode="--personal")
     print("generating passwords....")
     print(i.name) 
     print(i.currentyear) 
     print(i.address) 
 
-    i.generate(results, keywords, keynums)
+    i.generate(result, keywords, keynums)
     
 main()
