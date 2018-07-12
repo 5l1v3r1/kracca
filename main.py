f67adcb020ea79bb2f7f007c0c77cd5f3824ef3b
lmaocow <lolcow@protonmail.com>
2018-07-12 15:54:19 +0000

    coming soon

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from pyfiglet import Figlet
 from info import Info 
 import sys
 
+def hlp(): 
+    print("--enterprise     enterprise mode\n--personal     personal mode\n--caps-only     filters output for passwords that contain only capital letters\n--with-nums     filters output for passwords that contain numbers")
+
+
 def main(): 
     f = Figlet(font='slant')
     print(f.renderText('kracca'))
@@ -9,7 +13,7 @@ def main():
         print("must provide mode:\n--enterprise:    includes address and differing name permutations in results\n--personal:    includes DOB and family permutations")
         exit()
     elif len(sys.argv) != 6: 
-        print("--enterprise\n--personal\nformat: results.txt keywords.txt keynums.txt pooltxt.txt")
+        hlp()
         exit()
     else: 
         if sys.argv[1] == "--enterprise": 
@@ -17,7 +21,7 @@ def main():
         elif sys.argv[1] == "--personal": 
             print("====PERSONAL MODE====")
         if sys.argv[1] == "--help": 
-            print("--enterprise\n--personal\nformat: results.txt keywords.txt keynums.txt pooltxt.txt")
+            hlp()
             exit()
         result  = sys.argv[2] 
         keywords = sys.argv[3] 
