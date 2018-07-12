e50b76b7d15a4d727dcbfe878fd8876a32007022
lmaocow <lolcow@protonmail.com>
2018-07-12 18:52:14 +0000

    wew lad

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ def hlp():
 def main(): 
     f = Figlet(font='slant')
     print(f.renderText('kracca'))
-    if len(sys.argv) == 0:
-        print("must provide mode:\n--enterprise:    includes address and differing name permutations in results\n--personal:    includes DOB and family permutations")
-        exit()
-    elif len(sys.argv) != 6: 
+    if len(sys.argv) == 1:
         hlp()
         exit()
     else: 
@@ -23,14 +20,48 @@ def main():
         if sys.argv[1] == "--help": 
             hlp()
             exit()
+    if len(sys.argv) == 6: 
         result  = sys.argv[2] 
         keywords = sys.argv[3] 
         keynums = sys.argv[4]
         pooltxt = sys.argv[5]
-    if sys.argv[1] == "--enterprise": 
-        i = Info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise")
-    elif sys.argv[1] == "--personal":
-        i = Info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal")
+        if sys.argv[1] == "--enterprise": 
+            i = info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise")
+        elif sys.argv[1] == "--personal":
+            i = info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal")
+    elif len(sys.argv) == 7:
+        result  = sys.argv[2] 
+        keywords = sys.argv[3] 
+        keynums = sys.argv[4]
+        pooltxt = sys.argv[5]
+        filter1 = sys.argv[6]
+        if filter1 == "--caps-only":
+            if sys.argv[1] == "--enterprise": 
+                i = info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise", capitalfilter=filter1 )
+            elif sys.argv[1] == "--personal":
+                i = info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal", capitalfilter=filter1)
+        elif filter1 == "--with-nums": 
+            if sys.argv[1] == "--enterprise": 
+                i = info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise", numberfilter=filter1)
+            elif sys.argv[1] == "--personal":
+                i = info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal", numberfilter=filter1) 
+    elif len(sys.argv) == 8: 
+        result  = sys.argv[2] 
+        keywords = sys.argv[3] 
+        keynums = sys.argv[4]
+        pooltxt = sys.argv[5]
+        filter1 = sys.argv[6]
+        filter2 = sys.argv[7]
+        if filter1 == "--caps-only":
+            if sys.argv[1] == "--enterprise": 
+                i = info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise", capitalfilter=filter1, numberfilter=filter2)
+            elif sys.argv[1] == "--personal":
+                i = info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal", capitalfilter=filter1, numberfilter=filter2)
+        elif filter1 == "--with-nums": 
+            if sys.argv[1] == "--enterprise": 
+                i = info(name=input("name: "), currentyear=input("current year: "), address=input("address: "), motto=input("motto: "), phone=input("phone: "), email=input("email: "), mode="--enterprise", numberfilter=filter1, capitalfilter=filter2)
+            elif sys.argv[1] == "--personal":
+                i = info(name=input("name: "), aliases=input("aliases: "),address=input("address: "), phone=input("phone: "), email=input("emails: "), family=input("family names: "), mode="--personal", numberfilter=filter1, capitalfilter=filter2) 
     print("bl1ng bl1ng i c u ;)")
     i.generate(result, keywords, keynums, pooltxt)
     
